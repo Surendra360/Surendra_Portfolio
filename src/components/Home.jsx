a86cfbc3eb4bd67653e16086b00d42a9292324c2
Omta Dayma <76040920+omta-coder@users.noreply.github.com>
2024-09-26 23:26:43 +0530

    Update Home.jsx
    changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Footer from "./Footer";
 import { Link } from "react-router-dom";
-import HeroText from "./HeroText";
+import About from "./About";
+import Project from "./Project";
 
 const Home = () => {
   return (
@@ -38,11 +39,15 @@ const Home = () => {
           </section>
         </div>
           
-          <div className="py-10 ">
-          <HeroText />
-          </div>
+          
 
       </div>
+      <div >
+       <About/>
+          </div>
+      <div >
+       <Project/>
+          </div>
     </div>
   );
 };
